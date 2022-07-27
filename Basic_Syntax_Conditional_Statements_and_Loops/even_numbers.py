517ea1543e2a1d1499f291653728b82718af8960
Maria Mihaleva <mabrasheva1@gmail.com>
2022-07-27 23:23:35 +0300

    added other solution

--- Basic_Syntax_Conditional_Statements_and_Loops/even_numbers.py
+++ Basic_Syntax_Conditional_Statements_and_Loops/even_numbers.py
@@ -1,14 +1,10 @@
 numbers_count = int(input())
-odd_number = False
 
 for numbers in range(numbers_count):
     number = int(input())
 
     if number % 2 == 1:
-        odd_number = True
+        print(f"{number} is odd!")
         break
-
-if odd_number:
-    print(f"{number} is odd!")
 else:
     print("All numbers are even.")
